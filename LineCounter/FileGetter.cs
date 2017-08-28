9d2236f0c8e4eab38e584875812bd4899f3a1c4c
Kasper B. Graversen <kasg@mvno.dk>
2017-08-28 14:29:26 +0200

    recursive tree walker

---
+++ LineCounter/FileGetter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TeamBinary.LineCounter
+{
+	public class FileGetter
+	{
+		public IEnumerable<string> GetFile(string path, Func<string, bool> filter)
+		{
+			DirectoryInfo d = new DirectoryInfo(path);
+
+			var files = d.EnumerateFiles("*.*", SearchOption.TopDirectoryOnly).Select(x => x.FullName);
+			foreach (var file in files)
+			{
+				if(filter(file))
+					yield return file;
+			}
+
+			var dirs = d.EnumerateDirectories("*.*", SearchOption.TopDirectoryOnly).Select(x => x.FullName).ToArray();
+			foreach (var dir in dirs)
+			{
+				if(filter(dir))
+				{
+					foreach (var xx in GetFile(dir, filter))
+					{
+						yield return xx;
+					}
+				}
+			}
+		}
+	}
+}
