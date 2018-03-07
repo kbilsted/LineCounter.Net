54dac6947dd3fd144a2e011af32c48d4625d6bdc
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2018-03-07 22:30:23 +0100

    Add ReadMeHelper

---
+++ LineCounter.Tests/ReadMeHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using TeamBinary.LineCounter;
+
+namespace LineCounter.Tests
+{
+	public class ReadMeHelper
+	{
+		[Test]
+		public void MutateReadme() {
+			var pathReadme = @"C:\src\LineCounter.Net\README.md";
+			var readme = File.ReadAllText(pathReadme);
+			var stats = new DirWalker().DoWork(@"C:\src\LineCounter.Net\");
+
+			var shieldsRegEx = new Regex("<!--start-->.*<!--end-->", RegexOptions.Singleline);
+			var githubShields = new WebFormatter().CreateGithubShields(stats);
+			var newReadMe = shieldsRegEx.Replace(readme, "<!--start-->"+githubShields+ "<!--end-->");
+
+			if (readme!=newReadMe)
+				File.WriteAllText(pathReadme, newReadMe);
+		}
+	}
+}
