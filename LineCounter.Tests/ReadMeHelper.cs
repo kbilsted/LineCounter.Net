92779ef169e5d138247643c4b537d3b578360b8f
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2018-03-10 17:05:22 +0100

    update README and added tests for README

--- LineCounter.Tests/ReadMeHelper.cs
+++ LineCounter.Tests/ReadMeHelper.cs
@@ -1,24 +1,76 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
 using TeamBinary.LineCounter;
 
 namespace LineCounter.Tests
 {
-	public class ReadMeHelper
-	{
-		[Test]
-		public void MutateReadme() {
-			var pathReadme = @"C:\src\LineCounter.Net\README.md";
-			var readme = File.ReadAllText(pathReadme);
-			var stats = new DirWalker().DoWork(@"C:\src\LineCounter.Net\");
+    public class ReadMeHelper
+    {
+        [Test]
+        public void MutateReadme()
+        {
+            var basePath = Path.Combine(Assembly.GetExecutingAssembly().Location, "..", "..", "..","..");
 
-			var shieldsRegEx = new Regex("<!--start-->.*<!--end-->", RegexOptions.Singleline);
-			var githubShields = new WebFormatter().CreateGithubShields(stats);
-			var newReadMe = shieldsRegEx.Replace(readme, "<!--start-->"+githubShields+ "<!--end-->");
+            var stats = new DirWalker().DoWork(Path.Combine(basePath, "LineCounter"));
 
-			if (readme!=newReadMe)
-				File.WriteAllText(pathReadme, newReadMe);
-		}
-	}
+            var shieldsRegEx = new Regex("<!--start-->.*<!--end-->", RegexOptions.Singleline);
+            var githubShields = new WebFormatter().CreateGithubShields(stats);
+
+            var readmePath = Path.Combine(basePath, "README.md");
+            var oldReadme = File.ReadAllText(readmePath);
+            var newReadMe = shieldsRegEx.Replace(oldReadme, "<!--start-->" + githubShields + "<!--end-->");
+
+            if (oldReadme != newReadMe)
+                File.WriteAllText(readmePath, newReadMe);
+        }
+
+        [Test]
+        public void CountExample()
+        {
+            string version1 = @"
+    class Foo {
+        public void Bar() {
+            if(moons == 9) {
+                 Planet = Pluto;
+            } else {
+                 Planet = Mars;
+            }
+        }
+        public string Another() {
+            Console.WriteLine("" * "")
+        }
+    }
+";
+            string version2 = @"    
+class Foo 
+{
+    public void Bar() 
+    {
+        if(moons == 9) 
+        {
+            Planet = Pluto;
+        } 
+        else 
+        {
+            Planet = Mars;
+        }
+    }
+        
+    public string Another() 
+    {
+        Console.WriteLine("" * "")
+    }
+}
+    ";
+
+            var stat1 = new CSharpStrategy().Count(version1.Split('\n'));
+            var stat2 = new CSharpStrategy().Count(version2.Split('\n'));
+
+            Assert.AreEqual(8, stat1.CodeLines);
+            Assert.AreEqual(8, stat2.CodeLines);
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,11 +1,66 @@
 # LineCounter.Net
-Count lines of source code (in a meaning and configurable way) and can create nice-looking shields icons 
-<!--start-->[![Stats](https://img.shields.io/badge/Code_lines-324-ff69b4.svg)]()
-[![Stats](https://img.shields.io/badge/Doc_lines-1,6_K-ff69b4.svg)]()<!--end-->
+A library to do sensible line counting and generating "shields" for Github readme's.
 
-The shields looks like: [![Stats](https://img.shields.io/badge/Code_lines-324-ff69b4.svg)]()
-[![Stats](https://img.shields.io/badge/Doc_lines-1,6_K-ff69b4.svg)]()
-
+Project statistics:
+<!--start-->[![Stats](https://img.shields.io/badge/Code_lines-240-ff69b4.svg)]()
+[![Stats](https://img.shields.io/badge/Doc_lines-2-ff69b4.svg)]()<!--end-->
 
 
+
+## Sensible line counting
+What is "sensible line counting"? The idea is to count lines of code in such a way that 
+
+    * Only lines with semantic value are counted.
+    * Various coding styles for the same code should roughly yield the same line counting.
+    * Take into account "you get what you measure".
+    
+Why is that important? Because we want an estimate of size of code irregardles of coding style. For example
+
+    class Foo {
+        public void Bar() {
+            if(moons == 9) {
+                 Planet = Pluto;
+            } else {
+                 Planet = Mars;
+            }
+        }
+        public string Another() {
+            Console.WriteLine("*")
+        }
+    }
+
+Should roughly yield the same lines of code as the less compact style
+
+    class Foo 
+    {
+        public void Bar() 
+        {
+            if(moons == 9) 
+            {
+                 Planet = Pluto;
+            } 
+            else 
+            {
+                 Planet = Mars;
+            }
+        }
+        
+        public string Another() 
+        {
+            Console.WriteLine("*")
+        }
+    }
+
+
+In the text editor we have example 1 being 12 lines of code and example 2 19 lines or 58% longer! Linecounter will report 8 lines of code for each example.
+
+
+
+## You get what you measure
+
+The theory "you get what you measure" means exactly that. By whatever metric you measure, people will adapt seeking the easiest way to perform within the confinements of the measurements. 
+E.g. when you praise programmers for the number of lines of code, they'll start making long-winded code. 
+I've withnessed this first-hand a couple of times with outsourcing. 
+
+The code is far from perfect with respect to only counting semantic lines, but its a start :-)
 
